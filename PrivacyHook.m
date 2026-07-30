fc9b29b62d5966b73629e06f8ac918bbadcc51d3
sx6427 <sx82498@gmail.com>
2026-07-30 19:55:13 +0800

    fix: wrap genUUIDStr in block, remove unused var

--- PrivacyHook.m
+++ PrivacyHook.m
@@ -127,8 +127,8 @@ static NSURL *replaceDeviceParamsInURL(NSURL *url) {
         if ([item.name isEqualToString:@"cuid"] || [item.name hasPrefix:@"cuid_"] ||
             [item.name isEqualToString:@"cfrom"] || [item.name isEqualToString:@"c3_aid"] ||
             [item.name isEqualToString:@"idfa"] || [item.name isEqualToString:@"idfv"]) {
-            NSString *fakeVal = [item.name isEqualToString:@"idfa"] ? getPersistent(@"fake.idfa", genUUIDStr)
-                          : [item.name isEqualToString:@"idfv"] ? getPersistent(@"fake.idfv", genUUIDStr)
+            NSString *fakeVal = [item.name isEqualToString:@"idfa"] ? getPersistent(@"fake.idfa", ^{ return genUUIDStr(); })
+                          : [item.name isEqualToString:@"idfv"] ? getPersistent(@"fake.idfv", ^{ return genUUIDStr(); })
                           : getFakeID(@"cuid");
             [newItems addObject:[NSURLQueryItem queryItemWithName:item.name value:fakeVal]];
             modified = YES;
@@ -160,8 +160,8 @@ static NSData *replaceDeviceParamsInBody(NSData *body, NSString *contentType) {
                                 @"idfa", @"idfv", @"device_id", @"udid"];
         for (NSString *key in [json allKeys]) {
             if ([deviceKeys containsObject:key] || [key hasPrefix:@"cuid"]) {
-                NSString *fakeVal = [key isEqualToString:@"idfa"] ? getPersistent(@"fake.idfa", genUUIDStr)
-                              : [key isEqualToString:@"idfv"] ? getPersistent(@"fake.idfv", genUUIDStr)
+                NSString *fakeVal = [key isEqualToString:@"idfa"] ? getPersistent(@"fake.idfa", ^{ return genUUIDStr(); })
+                              : [key isEqualToString:@"idfv"] ? getPersistent(@"fake.idfv", ^{ return genUUIDStr(); })
                               : getFakeID(@"cuid");
                 json[key] = fakeVal;
                 modified = YES;
@@ -183,8 +183,8 @@ static NSData *replaceDeviceParamsInBody(NSData *body, NSString *contentType) {
             if (eqRange.location == NSNotFound) { [newPairs addObject:pair]; continue; }
             NSString *name = [pair substringToIndex:eqRange.location];
             if ([deviceKeys containsObject:name] || [name hasPrefix:@"cuid"]) {
-                NSString *fakeVal = [name isEqualToString:@"idfa"] ? getPersistent(@"fake.idfa", genUUIDStr)
-                              : [name isEqualToString:@"idfv"] ? getPersistent(@"fake.idfv", genUUIDStr)
+                NSString *fakeVal = [name isEqualToString:@"idfa"] ? getPersistent(@"fake.idfa", ^{ return genUUIDStr(); })
+                              : [name isEqualToString:@"idfv"] ? getPersistent(@"fake.idfv", ^{ return genUUIDStr(); })
                               : getFakeID(@"cuid");
                 [newPairs addObject:[NSString stringWithFormat:@"%@=%@", name, fakeVal]];
                 modified = YES;
@@ -308,7 +308,6 @@ static void initPrivacyHook(void) {
             // identifierForVendor → fake NSUUID
             Method idfvM = class_getInstanceMethod(deviceClass, @selector(identifierForVendor));
             if (idfvM) {
-                IMP idfvOrig = method_getImplementation(idfvM);
                 IMP idfvImp = imp_implementationWithBlock(^NSUUID *(id s) {
                     NSString *uuidStr = getPersistent(@"fake.idfv", ^NSString *{ return genUUIDStr(); });
                     return [[NSUUID alloc] initWithUUIDString:uuidStr];
